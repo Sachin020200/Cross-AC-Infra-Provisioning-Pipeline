6edf122fd8d4192a76a03f7e1a87f07a67d52ba8
Sachin020200 <155797714+Sachin020200@users.noreply.github.com>
2024-08-26 21:54:43 +0530

    Delete CICD/Templates directory

--- CICD/Templates/codecommit-repo.tf
+++
@@ -1,5 +0,0 @@
-#----------------- Data Block of existing Repo -----------------------------
-
-data "aws_codecommit_repository" "cross-account-infra-repo" {
-  repository_name = "cross-account-infra-repo"
-}
